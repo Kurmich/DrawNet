37dce5e20027874c420aaf369b52dca00da05b9d
Kurmich <k.kaiyrbekov@gmail.com>
2017-06-21 17:06:50 +0300

    optimized loss calculation

--- src/DrawNet.jl
+++ src/DrawNet.jl
@@ -34,14 +34,14 @@ V -> point vector size (i.e. 5 for (delta_x, delta_y, p1, p2, p3))
 =#
 function initencoder(model, H::Int, V::Int)
   #incoming input -> dims = (batchsize, V=5)
-  model[:fw_state0] = [initxav(1, H), initxav(1, H)]
+  model[:fw_state0] = [initxav(1, H), initzeros(1, H)]
   model[:fw_embed] = initxav(V, H) # x = input * model[:fw_embed]; x_dims = [batchsize, H]
   #here x and hidden will be concatenated form lstm_input with dims = [batchsize, H]
-  model[:fw_encode] = [ initxav(2H, 4H), initxav(1, 4H) ] #lstm_outdims = [batchsize, H]
+  model[:fw_encode] = [ initxav(2H, 4H), initzeros(1, 4H) ] #lstm_outdims = [batchsize, H]
   #same analysis goes for the decoder
-  model[:bw_state0] = [initxav(1, H), initxav(1, H)]
+  model[:bw_state0] = [initxav(1, H), initzeros(1, H)]
   model[:bw_embed] = initxav(V, H)
-  model[:bw_encode] = [ initxav(2H, 4H), initxav(1, 4H) ] #lstm_outdims = [batchsize, H]
+  model[:bw_encode] = [ initxav(2H, 4H), initzeros(1, 4H) ] #lstm_outdims = [batchsize, H]
 end
 
 #=
@@ -52,10 +52,10 @@ z_size -> size of latent vector z
 =#
 function initpredecoder(model, e_H::Int, d_H::Int, z_size::Int)
   #Incoming input dims = [batchsize, 2e_H]
-  model[:mu] = [initxav(2e_H, 1), initxav(1, 1)] #mu = input * W_mu .+ b_mu -> dims = [batchsize, 1]
-  model[:sigma] = [initxav(2e_H, 1), initxav(1, 1)] #sigma = input * W_sigma .+ b_sigma -> dims = [batchsize, 1]
+  model[:mu] = [initxav(2e_H, 1), initzeros(1, 1)] #mu = input * W_mu .+ b_mu -> dims = [batchsize, 1]
+  model[:sigma_cap] = [initxav(2e_H, 1), initzeros(1, 1)] #sigma = input * W_sigma .+ b_sigma -> dims = [batchsize, 1]
   #perform z = mu .+ sigma*N(0, I) -> z_dims = [batchsize, z_size]
-  model[:z] = [initxav(z_size, 2d_H), initxav(1, 2d_H)] # dec_H_0 = z*W_z .+ b_z -> dims = [batchsize, d_H]
+  model[:z] = [initxav(z_size, 2d_H), initzeros(1, 2d_H)] # dec_H_0 = z*W_z .+ b_z -> dims = [batchsize, d_H]
 end
 
 #=
@@ -69,17 +69,25 @@ function initdecoder(model, H::Int, V::Int, num_mixture::Int, z_size::Int)
   initxav(d...) = atype(xavier(d...))
   #incoming input dims = [batchsize, z_size + V]
   model[:embed] = initxav(z_size + V, H) # x = input * model[:embed]; x_dims = [batchsize, H]
-  model[:decode] = [ initxav(2H, 4H), initxav(1, 4H) ] #lstm_outdims = [batchsize, H]
-  ##model[:output] = [initxav(H, 6num_mixture + 3 ), initxav(1, 6num_mixture + 3 )] #output = lstm_out * W_output .+ b_output -> dims = [batchsize, 6*num_mixture + 3]
-  #mixture weights
-  model[:out_p] = [initxav(H, num_mixture), initxav(1, num_mixture)] #output = lstm_out * W_out_p .+ b_out_p -> dims = [batchsize, num_mixture]
-  #3 logits
-  model[:out_q] = [initxav(H, 3), initxav(1, 3)] #output = lstm_out * W_out_q .+ b_out_q -> dims = [batchsize, 3]
-  #distribution parameters 5 for each mixture
-  model[:out_params] = [initxav(H, 5num_mixture), initxav(1, 5num_mixture)] #output = lstm_out * W_out_params .+ b_out_params -> dims = [batchsize, 5*num_mixture]
+  model[:decode] = [ initxav(2H, 4H), initzeros(1, 4H) ] #lstm_outdims = [batchsize, H]
+  model[:output] = [initxav(H, 6num_mixture + 3 ), initzeros(1, 6num_mixture + 3 )] #output = lstm_out * W_output .+ b_output -> dims = [batchsize, 6*num_mixture + 3]
 end
 
 function lstm(param, state, input)
+  weight, bias = param
+  hidden, cell = state
+  h = size(hidden, 2)
+  gates = hcat(input, hidden) * weight .+ bias
+  forget = sigm(gates[:, 1:h])
+  ingate = sigm(gates[:, 1+h:2h])
+  outgate = sigm(gates[:, 1+2h:3h])
+  change = tanh(gates[:, 1+3h:4h])
+  cell = cell .* forget + ingate .* change
+  hidden = outgate .* tanh(cell)
+  return (hidden, cell)
+end
+
+function bilstm(param, state, input)
   weight, bias = param
   hidden, cell = state
   h = size(hidden, 2)
@@ -108,7 +116,6 @@ function vec_bivariate_prob(x1, x2, mu1, mu2, s1, s2, rho)
   norm2 = x2 .- mu2
   s1s2 = s1 .* s2
   z = (norm1./s1).*(norm1./s1) + (norm2./s2).*(norm2./s2) - ( (2 .* rho .*  (norm1 .* norm2)) ./ s1s2 )
-  #println("z[1] $(AutoGrad.getval(z[1]))")
   neg_rho = 1 .- rho.*rho
   prob = exp(-(z ./ (2.*neg_rho)) ) ./ (2*pi.*s1s2.*sqrt(neg_rho))
   return prob
@@ -118,18 +125,16 @@ function softmax(p, d)
   tmp = exp(p)
   return tmp ./ sum(tmp, d)
 end
+
 function s2s(model, data, seqlen, wkl; epsilon = 1e-6)
   #model settings
-  maxlen = maximum(seqlen)
-  num_mixture = size(model[:out_p][1], 2)
-  V = size(data[1], 2)
-  batchsize = size(data[1], 1)
-  #println("batchsize = $(batchsize)")
-  d_H = size(model[:embed], 2)
-  z_size = size(model[:z][1], 1)
-  #this ones are wrong
+  maxlen = maximum(seqlen) #maximum length of the input sequence
+  M = Int((size(model[:output][1], 2)-3)/6) #number of mixtures
+  (batchsize, V) = size(data[1])
+  d_H = size(model[:embed], 2) #decoder hidden unit size
+  z_size = size(model[:z][1], 1) #size of latent vector z
+  #Initialize states for forward-backward rnns
   statefw = initstate(batchsize, model[:fw_state0])
-  #println(size(statefw[1]))
   statebw = initstate(batchsize, model[:bw_state0])
   #forward encoder
   for i = 1:maxlen
@@ -137,80 +142,44 @@ function s2s(model, data, seqlen, wkl; epsilon = 1e-6)
     statefw = lstm(model[:fw_encode], statefw, input)
   end
   #backward encoder
-  for i=maxlen:-1:1
+  for i = maxlen:-1:1
     input = data[i]*model[:bw_embed]
     statebw = lstm(model[:bw_encode], statebw, input)
   end
 
   #predecoder step
-  (h_fw, c_fw) = statefw
-  (h_bw, c_bw) = statebw
-  #println("h_fw[1, 1] = $(h_fw[1, 1])")
-  h = hcat(h_fw, h_bw)
+  h = hcat(statefw[1], statebw[1]) #(h_fw, c_fw) = statefw, (h_bw, c_bw) = statebw
   mu = h*model[:mu][1] .+ model[:mu][2]
-  #TO DO: DIMANESIONS OF SIGMA AND GAUSSIAN
-  sigma_cap = h*model[:sigma][1] .+ model[:sigma][2]
+  sigma_cap = h*model[:sigma_cap][1] .+ model[:sigma_cap][2]
   sigma = exp( sigma_cap/2 )
-  #println("sizesigma = $(size(sigma)) sizemu= $(size(mu))")
-  z = mu .+ sigma .* atype(gaussian(batchsize, z_size;mean=0, std=1))
+  z = mu .+ sigma .* atype( gaussian(batchsize, z_size; mean=0, std=1) )
 
   #decoder step
   hc = tanh(z*model[:z][1] .+ model[:z][2])
-  #println(size(hc))
   state = (hc[:, 1:d_H], hc[:, d_H+1:2d_H])
   penstate_loss = 0
   offset_loss = 0
-  for i=2:maxlen
+  for i = 2:maxlen
     #dims data = [batchsize, V] = [batchsize, 5]
-  #  println("data[i-1][1,1] $(data[i-1][1,1])")
     input = hcat(data[i-1], z) #concatenate latent vector with previous point
     input = input * model[:embed]
     state = lstm(model[:decode], state, input)
-    #Calculate L_s on paper
-    #result1 = -tf.log(result1 + epsilon) # avoid log(0)
-    mix_coeff = predict(model[:out_p], state[1])
-  #  println("mix coeff 1 1 $(mix_coeff[1, 1])")
-    pnorm = softmax(mix_coeff, 2)
-  #  println("size of pnorm $(size(pnorm))")
-    mix_params = predict(model[:out_params], state[1]) #are parameters different for each sketch in batch? or same parameters for single batch? one for all seems reasonable CHANGE THIS
-  #  println("size of mix_params $(size(mix_params))")
-    #iterate over all mixtures
-    mixIdx = 0
-    mix_probs = pnorm[:, 1] .* vec_bivariate_prob(data[i][:, 1], data[i][:, 2], mix_params[:, mixIdx+1], mix_params[:, mixIdx+2], exp(mix_params[:, mixIdx+3]), exp(mix_params[:, mixIdx+4]), tanh(mix_params[:, mixIdx+5]))
-    for m = 2:num_mixture
-      mixIdx = 5*(m-1)
-      mix_probs = mix_probs .+ pnorm[:, m] .* vec_bivariate_prob(data[i][:, 1], data[i][:, 2], mix_params[:, mixIdx+1], mix_params[:, mixIdx+2], exp(mix_params[:, mixIdx+3]), exp(mix_params[:, mixIdx+4]), tanh(mix_params[:, mixIdx+5]))
-    end
-    #println("mix_probs[1] $(AutoGrad.getval(mix_probs[1]))")
-  #  println("offset_loss $(AutoGrad.getval(offset_loss))")
-    offset_loss += -sum( log(mix_probs.+ epsilon ) )
-    #=
-    for j = 1:batchsize
-      mix_probs = 0
-      for m = 1:num_mixture
-        #are mix params different for each sketch point in batch ? NEED TO CLARIFY THIS.
-        #result1 = -tf.log(result1 + epsilon) # avoid log(0)
-        mixIdx = 5*(m-1)
-  #      println("mix weight $(AutoGrad.getval(pnorm[j, m]))")
-        mix_probs += pnorm[j, m] * bivariate_prob(data[i][j, 1], data[i][j, 2], mix_params[j, mixIdx+1], mix_params[j, mixIdx+2], exp(mix_params[j, mixIdx+3]), exp(mix_params[j, mixIdx+4]), tanh(mix_params[j, mixIdx+5])) ##vector scalar ? handle this part ?
-      end
-  #    println("sum of mix probs $(AutoGrad.getval(mix_probs))")
-      offset_loss += -log(mix_probs + epsilon)
-    end=#
-
-    #Calculate L_p in paper
-    logits = predict(model[:out_q], state[1])
-    qnorm = logp(logits, 2)
-  #  println("size of qnorm $(size(qnorm))")
-  #  println("size of data[i] $(size(data[i])) ")
-    penstate_loss += -sum(data[i][:, (V-2):V] .* qnorm)
+    output =  predict(model[:output], state[1]) #get output params
+    pnorm = softmax(output[:, 1:M], 2) #normalized distribution probabilities
+    mu_x = output[:, M+1:2M]
+    mu_y = output[:, 2M+1:3M]
+    sigma_x = exp(output[:, 3M+1:4M])
+    sigma_y = exp(output[:, 4M+1:5M])
+    rho = tanh(output[:, 5M+1:6M])
+    qnorm = logp(output[:, 6M+1:6M+3], 2) #normalized logit values
+    mix_probs = pnorm .* vec_bivariate_prob(data[i][:, 1], data[i][:, 2], mu_x, mu_y, sigma_x, sigma_y, rho)
+    offset_loss += -sum( log( sum(mix_probs, 2).+ epsilon ) ) #L_s on paper(add epsilon to avoid log(0))
+    penstate_loss += -sum(data[i][:, (V-2):V] .* qnorm) #L_p on paper
   end
-  kl_loss = -sum((1 + sigma_cap + mu.*mu - exp(sigma_cap))) / (2*z_size*batchsize)
+  kl_loss = -sum((1 + sigma_cap - mu.*mu - exp(sigma_cap))) / (2*z_size*batchsize) #Kullback-Leibler divergence loss term
   offset_loss /= (maxlen * batchsize)
   penstate_loss /= (maxlen * batchsize)
-  #println("size kl_loss = $(size(kl_loss)) size offset_loss = $(size(offset_loss)) size penstate_loss = $(size(penstate_loss))")
   loss = offset_loss + penstate_loss + wkl*kl_loss
-  #println("loss = $(AutoGrad.getval(loss)) ")
   return loss
 end
 
@@ -234,7 +203,13 @@ function train(model, data, seqlens, wkl, opts, epochs)
       update!(model, grads, opts)
     end
     @printf("epoch: %d trn loss: %g\n", e , avgloss(model, data, seqlens, wkl))
+    if e%20==0
+      flush(STDOUT)
+      arrmodel = convertmodel(model)
+      save("model$(e).jld","model", arrmodel)
+    end
   end
+
 end
 
 function avgloss(model, data, seqlens, wkl)
@@ -275,12 +250,24 @@ initoptim{T<:Number}(::Array{T},otype)=eval(parse(otype))
 initoptim(a::Associative,otype)=Dict(k=>initoptim(v,otype) for (k,v) in a)
 initoptim(a,otype)=map(x->initoptim(x,otype), a)
 
+
+# convert model to save
+convertmodel{T<:Number}(x::KnetArray{T}) = convert(Array{T}, x)
+convertmodel{T<:Number}(x::Array{T}) = convert(Array{T}, x)
+convertmodel(a::Associative)=Dict(k=>convertmodel(v) for (k,v) in a)
+convertmodel(a) = map(x->convertmodel(x), a)
+
+# reverse model converted for loading from file
+revconvertmodel{T<:Number}(x::Array{T}) = convert(KnetArray{T}, x)
+revconvertmodel(a::Associative) = Dict(k=>revconvertmodel(v) for (k, v) in a)
+revconvertmodel(a) = map(x->revconvertmodel(x), a)
+
 function main(args=ARGS)
   s = ArgParseSettings()
   s.description="A Neural Representation of Sketch Drawings. (c) Kurmanbek Kaiyrbekov 2017."
   s.exc_handler=ArgParse.debug_handler
   @add_arg_table s begin
-    ("--epochs"; arg_type=Int; default=10; help="Total number of training set. Keep large.")
+    ("--epochs"; arg_type=Int; default=100; help="Total number of training set. Keep large.")
     ("--save_every"; arg_type=Int; default=500; help="Number of batches per checkpoint creation.")
     ("--dec_model"; arg_type=String; default="lstm"; help="Decoder: lstm, or ....")
     ("--filename"; arg_type=String; default="full_simplified_airplane.ndjson"; help="Data file name")
@@ -292,7 +279,7 @@ function main(args=ARGS)
     ("--num_mixture"; arg_type=Int; default=20; help="Number of mixtures in Gaussian mixture model.")
     ("--z_size"; arg_type=Int; default=128; help="Size of latent vector z. Recommend 32, 64 or 128.")
     ("--V"; arg_type=Int; default=5; help="Number of elements in point vector.")
-    ("--wkl"; arg_type=Float32; default=1.0; help="Parameter weight for Kullback-Leibler loss.")
+    ("--wkl"; arg_type=Float64; default=1.0; help="Parameter weight for Kullback-Leibler loss.")
     ("--readydata"; action=:store_true; help="is data preprocessed and ready")
     ("--testmode"; action=:store_true; help="true if in test mode")
     ("--pretrained"; action=:store_true; help="true if pretrained model exists")
