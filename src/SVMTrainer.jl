825014819d2d0bd18ef3093c17899176a7bba676
Kurmich <k.kaiyrbekov@gmail.com>
2017-08-17 20:17:11 +0300

    functional svm trainer

--- src/SVMTrainer.jl
+++ src/SVMTrainer.jl
@@ -67,26 +67,109 @@ function randindinces(annotations)
   return indices
 end
 
-function trainsvm(annotations)
-  CS = [1.0 2.0 4.0 8.0 16.0 1e2 5e2 1e3]
-  GAMMAS = [0.5 0.25 0.125]
-  tstacc = 0
-  for C in CS
-    for gamma in GAMMAS
-      svmmodel = SVR.train(trnlabels, trnfeats; svm_type=SVR.C_SVC, kernel_type=SVR.RBF, C=C, gamma=gamma)
-      acc = getaccuracy(svmmodel, tstfeats, tstlabels)
-      tstacc = max(acc, tstacc)
-      println("C=$(C), tst acc=$(tstacc)")
-      SVR.freemodel(svmmodel)
+function train_test_split(data, tstsize; indices = nothing)
+  @assert(tstsize > 0 && tstsize < 1, "tstsize should be in range 0 < tstsize < 1")
+  indices = (indices == nothing) ? randindinces(data) : indices
+  trndata = Dict()
+  tstdata = Dict()
+  for label in keys(data)
+    idx = indices[label]
+    curdata = data[label]
+    up = Int(floor(tstsize*length(idx)))
+    tstdata[label] = curdata[idx[1:up]]
+    trndata[label] = curdata[idx[up+1:length(idx)]]
+  end
+  return trndata, tstdata
+end
+
+function shift_indices!(indices, vldsize)
+  for label in keys(indices)
+    idx = indices[label]
+    up = Int(floor(vldsize*length(idx)))
+    indices[label] = circshift(idx, up)
+  end
+end
+
+function get_feats_and_classes(data, labels)
+  feats = nothing
+  classes = []
+  for i = 1:length(labels)
+    key = labels[i]
+    for feat in data[key]
+      if feats == nothing
+        feats = feat
+      else
+        feats = vcat(feats, feat)
+      end
+      push!(classes, i)
     end
   end
+  #info("Size of feats = $(size(feats)) classes = $(size(classes))")
+  classes = convert(Array{Float64}, classes)
+  return feats', classes
+end
+function crossvalidate(data, cv, labels, C, gamma)
+  @assert(cv > 1, "Cross validation parameter should be greater than 1")
+  #info("Cross validating for C = $(C) and gamma = $(gamma)")
+  indices = randindinces(data)
+  vldsize = 1/cv
+  scores = []
+  for i = 1:cv
+    shift_indices!(indices, vldsize)
+    trndata, vlddata = train_test_split(data, vldsize; indices = indices)
+    trnfeats, trnlabels = get_feats_and_classes(trndata, labels)
+    vldfeats, vldlabels = get_feats_and_classes(vlddata, labels)
+    svmmodel = SVR.train(trnlabels, trnfeats; svm_type=SVR.C_SVC, kernel_type=SVR.RBF, C=C, gamma=gamma)
+    acc = getaccuracy(svmmodel, vldfeats, vldlabels)
+    push!(scores, acc)
+    SVR.freemodel(svmmodel)
+  end
+  return scores
+end
+
+function get_cvd_params(data, cv, labels)
+  #search for best cross validation parameters
+  CS = []
+  GAMMAS = []
+  for i=1:11
+    push!(CS, 2.0^i)
+    push!(GAMMAS, 2.0^(-i))
+  end
+  bestC, bestGamma, bestscore = 0, 0, 0
+  for C in CS
+    for gamma in GAMMAS
+      scores = crossvalidate(data, cv, labels, C, gamma)
+      avgscore = sum(scores) / length(scores)
+      if avgscore > bestscore
+        bestC = C
+        bestGamma = gamma
+        bestscore = avgscore
+      end
+      println(scores)
+      @printf("C: %g gamma: %g avgscore: %g \n \n", C, gamma,  avgscore)
+    end
+  end
+  @printf("best C: %g best gamma: %g best avg score: %g \n", bestC, bestGamma,  bestscore)
+  return bestC, bestGamma
+end
+
+function trainsvm(trndata, tstdata, C, gamma, labels)
+  trnfeats, trnlabels = get_feats_and_classes(trndata, labels)
+  tstfeats, tstlabels = get_feats_and_classes(tstdata, labels)
+  svmmodel = SVR.train(trnlabels, trnfeats; svm_type=SVR.C_SVC, kernel_type=SVR.RBF, C=C, gamma=gamma)
+  acc = getaccuracy(svmmodel, tstfeats, tstlabels)
+  @printf("best C: %g best gamma: %g tst accuracy: %g \n", C, gamma, acc)
+  SVR.savemodel(svmmodel, "airplane.model")
+  SVR.freemodel(svmmodel)
 end
 
 function main(args=ARGS)
   s = ArgParseSettings()
-  s.description="SVM TRAINER. (c) Kurmanbek Kaiyrbekov 2017."
+  s.description="SVM model trainer. (c) Kurmanbek Kaiyrbekov 2017."
   s.exc_handler=ArgParse.debug_handler
   @add_arg_table s begin
+    ("--tstsize"; arg_type=Float64; default=0.2; help="Test set proportion.")
+    ("--cv"; arg_type=Int; default=5; help="Cross validation fold parameter.")
     ("--datapath"; arg_type=String; default="../annotateddata/"; help="Number of epochs per checkpoint creation.")
     ("--filename"; arg_type=String; default="r_full_simplified_airplane.ndjson"; help="Decoder: lstm, or ....")
   end
@@ -97,7 +180,10 @@ function main(args=ARGS)
   labels = [ "UpW", "LoW", "F", "FWSR", "FWSL", "LS", "RS","LW", "RW", "O"]
   annotations = getdata(filename, labels)
   printdatastats(annotations)
-  getfeats(annotations)
+  idms = getfeats(annotations)
+  trnidms, tstidms = train_test_split(idms, o[:tstsize])
+  C, gamma = get_cvd_params(trnidms, o[:cv], labels)
+  trainsvm(trnidms, tstidms, C, gamma, labels)
 end
 
 if VERSION >= v"0.5.0-dev+7720"
